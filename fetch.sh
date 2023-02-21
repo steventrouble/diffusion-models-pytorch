98277412f3d5d47176c82fc1d587cc4bb63fe249
Steven Weiss <steventrouble@gmail.com>
2023-02-20 22:37:13 -0800

    Create fetch.sh

---
+++ fetch.sh
@@ -0,0 +1,36 @@
+# Fetches files from gutenberg
+
+mkdir -p jane_austen; cd jane_austen
+wget --no-clobber -q https://www.gutenberg.org/cache/epub/158/pg158.txt
+wget --no-clobber -q https://www.gutenberg.org/files/161/161-0.txt
+wget --no-clobber -q https://www.gutenberg.org/cache/epub/105/pg105.txt
+wget --no-clobber -q https://www.gutenberg.org/cache/epub/121/pg121.txt
+wget --no-clobber -q https://www.gutenberg.org/cache/epub/141/pg141.txt
+wget --no-clobber -q https://www.gutenberg.org/cache/epub/42671/pg42671.txt
+wget --no-clobber -q https://www.gutenberg.org/cache/epub/946/pg946.txt
+cd ..
+
+mkdir -p charles_dickens; cd charles_dickens
+wget --no-clobber -q https://www.gutenberg.org/files/675/675-0.txt
+wget --no-clobber -q https://www.gutenberg.org/cache/epub/25985/pg25985.txt
+wget --no-clobber -q https://www.gutenberg.org/cache/epub/40723/pg40723.txt
+wget --no-clobber -q https://www.gutenberg.org/cache/epub/44578/pg44578.txt
+wget --no-clobber -q https://www.gutenberg.org/files/46804/46804-0.txt
+wget --no-clobber -q https://www.gutenberg.org/cache/epub/1023/pg1023.txt
+wget --no-clobber -q https://www.gutenberg.org/cache/epub/28198/pg28198.txt
+wget --no-clobber -q https://www.gutenberg.org/cache/epub/23765/pg23765.txt
+wget --no-clobber -q https://www.gutenberg.org/cache/epub/37121/pg37121.txt
+wget --no-clobber -q https://www.gutenberg.org/files/42232/42232-0.txt
+wget --no-clobber -q https://www.gutenberg.org/cache/epub/699/pg699.txt
+wget --no-clobber -q https://www.gutenberg.org/files/653/653-0.txt
+wget --no-clobber -q https://www.gutenberg.org/cache/epub/821/pg821.txt
+wget --no-clobber -q https://www.gutenberg.org/cache/epub/1422/pg1422.txt
+wget --no-clobber -q https://www.gutenberg.org/files/1400/1400-0.txt
+wget --no-clobber -q https://www.gutenberg.org/files/809/809-0.txt
+wget --no-clobber -q https://www.gutenberg.org/files/644/644-0.txt
+wget --no-clobber -q https://www.gutenberg.org/files/927/927-0.txt
+wget --no-clobber -q https://www.gutenberg.org/files/580/580-0.txt
+wget --no-clobber -q https://www.gutenberg.org/files/924/924-0.txt
+wget --no-clobber -q https://www.gutenberg.org/files/1289/1289-0.txt
+wget --no-clobber -q https://www.gutenberg.org/files/98/98-0.txt
+wget --n
